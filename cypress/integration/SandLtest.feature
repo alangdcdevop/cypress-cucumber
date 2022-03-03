ee4301c1cbe92a00ec6a58ab44d9ac02e1dd3435
Alan Gabriel Duarte Carvalhosa <alan2015@hotmail.com>
2022-03-03 11:16:41 -0500

    feat: asd

--- cypress/integration/SandLtest.feature
+++ cypress/integration/SandLtest.feature
@@ -1,13 +1,21 @@
+# Feature: Google Main Page
+
+#   I want to open a search engine
+  
+#   Scenario: Opening a search engine page
+#     Given I open "www.google.com" url
+#     Then I complete field "input" with "Cartman"
+
+#     # input
+
+#     # Eric Cartman | Wiki South Park
+#     # body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b
+    
 Feature: Google Main Page
 
   I want to open a search engine
   
+  @focus
   Scenario: Opening a search engine page
-    Given I open "www.google.com" url
-    Then I complete field "input" with "Cartman"
-
-    # input
-
-    # Eric Cartman | Wiki South Park
-    # body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b
-    
+    Given I open Google page
+    Then I see "Google" in the title
--- cypress/support/step_definitions/step_sandl.js
+++ cypress/support/step_definitions/step_sandl.js
@@ -1,7 +1,28 @@
-import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+// import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 
+// const url=""
 
-Given("I open {url} url", (url) => {
+import { Given } from "cypress-cucumber-preprocessor/steps";
+import { Then } from "cypress-cucumber-preprocessor/steps";
+
+Then(`I see {string} in the title`, (title) => {
+  // cy.title().should('include', title)
+  // cy.get('.gLFyf').click()
+  cy.get('.gLFyf').type("Cartman")
+  cy.get('.CqAVzb > center > .gNO89b').click()
+ 
+})
+const url = 'https://google.com'
+Given('I open Google page', () => {
+  cy.visit(url)
+
+})
+
+Then(`I see {string} in the title`, (title) => {
+  cy.title().should('include', title)
+})
+
+Given("I open {string} url", (url) => {
   cy.visit(url);
 });
 
@@ -9,14 +30,14 @@ Given("I click on {string} button",  (button) => {
   cy.get(button).click() // Click on button
 });
 
-Given("I complete field {field} with {data}",  (field, data) => {
+Given("I complete field {string} with {string}",  (field, data) => {
   cy.get(field).type(data)
 });
 
 
 
 
-Then("I open {url} url", (url) => {
+Then("I open {string} url", (url) => {
   cy.visit(url);
 });
 
@@ -24,12 +45,12 @@ Then("I click on {string} button",  (button) => {
   cy.get(button).click() // Click on button
 });
 
-Then("I complete field {field} with {data}",  (field, data) => {
+Then("I complete field {string} with {string}",  (field, data) => {
   cy.get(field).type(data)
 });
 
 
-When("I open {url} url", (url) => {
+When("I open {string} url", (url) => {
   cy.visit(url);
 });
 
@@ -37,7 +58,11 @@ When("I click on {string} button",  (button) => {
   cy.get(button).click() // Click on button
 });
 
-When("I complete field {field} with {data}",  (field, data) => {
+When("I complete field {string} with {string}",  (field, data) => {
   cy.get(field).type(data)
 });
 
+
+// cy.get('.gLFyf')
+
+// cy.get('.FPdoLc > center > .gNO89b')
